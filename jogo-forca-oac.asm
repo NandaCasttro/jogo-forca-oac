fe9c3c791286eff1147aa2ed2eef9962097fd55a
NandaCasttro <devnandacasttro@gmail.com>
2026-02-12 18:18:09 -0300

    Implementa modo 2 jogadores e reorganiza estrutura

--- jogo-forca-oac.asm
+++ jogo-forca-oac.asm
@@ -4,42 +4,25 @@
 
 .data
 
-# -------- Banco de Palavras --------
-p1: .asciiz "cache"
-p2: .asciiz "memoria"
-p3: .asciiz "processador"
-p4: .asciiz "registrador"
-p5: .asciiz "pipeline"
-p6: .asciiz "barramento"
-p7: .asciiz "assembly"
-p8: .asciiz "multiciclo"
-p9: .asciiz "computadores"
-p10: .asciiz "programacao"
+# -------- Variaveis --------
+palavra_secreta:  .space 40
+palavra_exibida:  .space 40
+letra_digitada:   .byte 0
 
-vetor_palavras:
-    .word p1
-    .word p2
-    .word p3
-    .word p4
-    .word p5
-    .word p6
-    .word p7
-    .word p8
-    .word p9
-    .word p10
-
-# -------- Variáveis do Jogo --------
-palavra_escolhida: .word 0
-palavra_exibida:   .space 30
-letra_digitada:    .byte 0
-erros:             .word 0
-max_erros:         .word 6
+erros:            .word 0
+max_erros:        .word 6
 
 # -------- Mensagens --------
-msg_inicio:    .asciiz "\n=== JOGO DA FORCA ===\n"
+msg_inicio:    .asciiz "\n=== JOGO DA FORCA - OAC ===\n"
+msg_j1:        .asciiz "\nJogador 1, digite a palavra secreta: "
+msg_j2:        .asciiz "\nJogador 2, Qual a palavra?\n"
 msg_letra:     .asciiz "\nDigite uma letra: "
 msg_vitoria:   .asciiz "\nVITORIA!\n"
 msg_derrota:   .asciiz "\nDERROTA!\n"
+msg_palavra:   .asciiz "\nA palavra era: "
+
+quebra_linha:  .asciiz "\n"
+
 
 ############################
 #           TEXT           #
@@ -59,72 +42,81 @@ main:
     la $a0, msg_inicio
     syscall
 
-    # Seleciona palavra aleatória
-    jal select_palavra
+    jal ler_palavra              # ler palavra do jogador 1
 
-    # Inicializa palavra exibida com "_"
-    jal inicia_palavra
+    jal inicializar_palavra      # Inicializa palavra exibida com "_"
+
+    sw $zero, erros              # Resetar erros
+
+    # Avisar Jogador 2
+    li $v0, 4
+    la $a0, msg_j2
+    syscall
+    
+#################################################
+#               LOOP PRINCIPAL                  #
+#################################################
 
 loop_jogo:
 
-    # Mostrar palavra atual
-    jal mostrar_palavra
+    jal mostrar_palavra     # Mostrar palavra atual
 
-    # Pedir letra ao usuário
-    jal ler_letra
+    jal ler_letra           # Pedir letra ao usuário
 
-    # Verificar letra
-    jal verificar_letra
+    jal verificar_letra     # Verificar letra
 
-    # Verificar fim de jogo
-    jal verificar_fim
+    jal verificar_fim       # Verificar fim de jogo
 
     j loop_jogo
 
 
 #################################################
-#           SELECIONAR PALAVRA                  #
+#           LER PALAVRA (JOGADOR 1)             #
 #################################################
 
-selecionar_palavra:
-    # Aqui vamos:
-    # 1. Gerar número aleatório 0-9 
-    li $v0, 42
-    li $a1, 10
+ler_palavra:
+    # Mostrar mensagem
+    li $v0, 4
+    la $a0, msg_j1
     syscall
-    
-    #Resultado está em $a0
-    move $t0, $a0      #guarda índice em $t0
-    
-    # 2. Multiplicar por 4
-    li $t1, 4
-    mul $t0, $t0, $t1
-    
-    # 3. Buscar endereço no vetor
-    #Carregar endereço base do vetor
-    la $t2, vetor_palavras
 
-    #Somar deslocamento
-    add $t2, $t2, $t0
-
-    #Carregar endereço da palavra sorteada
-    lw $t3, 0($t2)
-   
-    # 4. Salvar em palavra_escolhida
-    sw $t3, palavra_escolhida
+    # Ler string
+    li $v0, 8
+    la $a0, palavra_secreta
+    li $a1, 40
+    syscall
 
     jr $ra
 
 
 #################################################
-#        INICIALIZAR PALAVRA EXIBIDA
+#        INICIALIZAR PALAVRA EXIBIDA            #
 #################################################
 
 inicializar_palavra:
-    # Percorrer palavra escolhida
-    # Copiar "_" para palavra_exibida
-    # Parar no '\0'
 
+    la $t0, palavra_secreta    # ponteiro origem
+    la $t1, palavra_exibida    # ponteiro destino
+
+loop_init:
+
+    lb $t2, 0($t0)             # carrega caractere atual
+
+    beq $t2, 10, fim_init      # se '\n' -> parar
+    beq $t2, $zero, fim_init   # se '\0' -> parar
+
+    li $t3, 95                 # '_'
+    sb $t3, 0($t1)
+
+    addi $t0, $t0, 1
+    addi $t1, $t1, 1
+
+    j loop_init
+
+fim_init:
+
+    sb $zero, 0($t1)          # finaliza string com '\0'
+ 
     jr $ra
 
 
